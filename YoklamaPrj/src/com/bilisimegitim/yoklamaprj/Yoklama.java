31be76b7a22a864d831e7496b8a64494be1fd7f8
bogac <bogac@DESKTOP-05IMPEI>
2018-04-29 16:44:56 +0300

    İlk clas Yoklama.java eklendi.

---
+++ YoklamaPrj/src/com/bilisimegitim/yoklamaprj/Yoklama.java
@@ -0,0 +1,20 @@
+package com.bilisimegitim.yoklamaprj;
+
+public class Yoklama {
+
+	public static void main(String[] args) {
+		
+       String[] isimler = {"Bora", "Esra", "Berna", "Sertaç", "Boðaç", "Taner", "Yunus", "Gülþah" };  
+       
+       for (int i = 0; i < isimler.length; i++) {
+		System.out.println(isimler[i]);
+		
+		
+	}
+       
+       
+       
+       
+	}
+
+}
